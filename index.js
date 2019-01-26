6676db2ba18ff45b0a288b245a17a633dac065f0
Jeffrey T Gosselin <jgoz.email@gmail.com>
2019-01-26 21:30:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // Write your code in this file!
 function scuberGreetingForFeet(distance) {
+  let response;
   if (distance <= 400) {
-    return 'This one is on me!';
+    response = 'This one is on me!';
   } else if (distance > 2000 && distance < 2500) {
-    return 'I will gladly take your thirty bucks.';
+    response = 'I will gladly take your thirty bucks.';
   } else if (distance > 2500) {
-    return 'No can do.';
+    response = 'No can do.';
   }
+  return response;
 }
 
 
