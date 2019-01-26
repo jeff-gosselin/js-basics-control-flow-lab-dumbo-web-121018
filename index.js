28154598abcb059789268c5fad5bb4f3b9ba3f09
Jeffrey T Gosselin <jgoz.email@gmail.com>
2019-01-26 21:25:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function scuberGreetingForFeet(distance) {
 }
 
 
-function ternaryCheckCity(city) {
-  city === 'NYC' ? return 'Ok, sounds good.' : return 'No go.';
-}
+// function ternaryCheckCity(city) {
+//   city === 'NYC' ? return 'Ok, sounds good.' : return 'No go.';
+// }
